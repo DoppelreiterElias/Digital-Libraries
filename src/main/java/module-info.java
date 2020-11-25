8d10b712edc6ee2b0df874a6c28dabf8a4a0e131
instantcake <instcake@protonmail.com>
2020-11-25 22:38:12 +0100

    updated module-info.java and pom.xml with java jung support

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,11 @@ module tugraz.digitallibraries {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.xml;
+    requires jung.graph.impl; //conflicts with jung.api
+    requires jung.visualization;
+    requires jung.algorithms;
+    ///requires jung.api;
+    requires jung.io;
 
     opens tugraz.digitallibraries to javafx.fxml;
     exports tugraz.digitallibraries;
