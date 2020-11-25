a68c63adba04705c87eb6e9a233e5341b66550fb
instantcake <instcake@protonmail.com>
2020-11-25 21:51:57 +0100

    updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module tugraz.digitallibraries {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.xml;
 
     opens tugraz.digitallibraries to javafx.fxml;
     exports tugraz.digitallibraries;
