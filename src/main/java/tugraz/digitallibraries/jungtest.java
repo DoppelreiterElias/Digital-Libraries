092068a3f5bc39c021111f56ba4a63367cde8517
instantcake <instcake@protonmail.com>
2020-11-25 22:39:02 +0100

    jungtest added

---
+++ src/main/java/tugraz/digitallibraries/jungtest.java
@@ -0,0 +1,11 @@
+package tugraz.digitallibraries;
+
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+
+public class jungtest
+{
+    void test()
+    {
+        DirectedSparseGraph g = new DirectedSparseGraph();
+    }
+}
