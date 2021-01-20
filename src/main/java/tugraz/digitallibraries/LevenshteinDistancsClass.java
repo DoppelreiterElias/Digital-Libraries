a58a51c29ba3e10853fd382c0b67bd011fc241a2
DoppelreiterElias <doppelreiter@student.tugraz.at>
2021-01-20 16:21:36 +0100

    Update Searcher

--- src/main/java/tugraz/digitallibraries/LevenshteinDistancsClass.java
+++ src/main/java/tugraz/digitallibraries/LevenshteinDistancsClass.java
@@ -1,8 +1,135 @@
 package tugraz.digitallibraries;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import tugraz.digitallibraries.graph.EdgeCoAuthorship;
+import tugraz.digitallibraries.ui.DetailViewObject;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LevenshteinDistancsClass {
+
+
+    static public ObservableList<DetailViewObject> paperTitelLevenshteinDistances(String target, ArrayList<EdgeCoAuthorship> edges_list, int number_of_hits)
+    {
+
+        ObservableList<DetailViewObject> return_list = FXCollections.observableArrayList();
+
+        List<String> text_list = new ArrayList<String>();
+
+
+        target = "[" + target + "]";
+
+
+        for (int i = 0; i < edges_list.size(); i++)
+        {
+            String new_paper = edges_list.get(i).getPapers().toString();
+
+            boolean already_in_array = false;
+
+
+           for (int j = 0; j < i ; j++)
+           {
+               if (new_paper.equals(edges_list.get(j).getPapers().toString()))
+               {
+                   already_in_array = true;
+               }
+           }
+
+            if (!already_in_array)
+            {
+                text_list.add(edges_list.get(i).getPapers().toString());
+            }
+        }
+
+
+
+        //List to Array
+        String unsorted_texts[] = new String[text_list.size()];
+        String sorted_texts[] = new String[text_list.size()];
+
+        unsorted_texts = text_list.toArray(unsorted_texts);
+
+
+        int sorted_ratings[] = new int[text_list.size()];
+
+        int rating[] = new int[text_list.size()];
+
+        for (int i = 0; i < unsorted_texts.length; i++)
+        {
+            rating[i] = calculateLevenshteinDistance(target,unsorted_texts[i]);
+        }
+
+
+        int max = 0;
+
+        for (int i = 0; i < rating.length;i++)
+        {
+            if (max < rating[i])
+                max = rating[i];
+        }
+
+        int temporary_holder[] = new int[max+1];
+
+
+        for (int i = 0; i < rating.length; i++)
+        {
+            temporary_holder[rating[i]] += 1;
+        }
+
+
+        for (int i = 1; i < temporary_holder.length; i++)
+        {
+            temporary_holder[i] = temporary_holder[i] + temporary_holder[i-1];
+        }
+
+
+        for (int i = 0; i < rating.length;i++)
+        {
+            sorted_ratings[temporary_holder[rating[i]]-1] = rating[i];
+            sorted_texts[temporary_holder[rating[i]]-1] = unsorted_texts[i];
+            temporary_holder[rating[i]] -= 1;
+        }
+
+
+
+        if (number_of_hits >= unsorted_texts.length)
+        {
+            for (int i = 0; i < sorted_texts.length; i++)
+            {
+                for (int j = 0; j < edges_list.size(); j++)
+                {
+                    if (sorted_texts[i].equals(edges_list.get(j).getPapers().toString()))
+                    {
+                        return_list.addAll(edges_list.get(j).getPapers());
+                        break;
+                    }
+                }
+            }
+
+        } else if (number_of_hits < 0)
+        {
+            return_list = null;
+        }
+        else
+        {
+            for (int i = 0; i < number_of_hits; i++)
+            {
+                for (int j = 0; j < edges_list.size(); j++)
+                {
+                    if (sorted_texts[i].equals(edges_list.get(j).getPapers().toString()))
+                    {
+                        return_list.addAll(edges_list.get(j).getPapers());
+                        break;
+                    }
+                }
+            }
+        }
+
+        return return_list;
+    }
 
 
     //based on pseudo code: https://de.wikipedia.org/wiki/Countingsort
--- src/main/java/tugraz/digitallibraries/Searcher.java
+++ src/main/java/tugraz/digitallibraries/Searcher.java
@@ -3,6 +3,7 @@ package tugraz.digitallibraries;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import tugraz.digitallibraries.dataclasses.Author;
+import tugraz.digitallibraries.dataclasses.MetadataEntry;
 import tugraz.digitallibraries.graph.EdgeCoAuthorship;
 import tugraz.digitallibraries.graph.GraphCreator;
 import tugraz.digitallibraries.ui.DetailViewObject;
@@ -18,10 +19,6 @@ public class Searcher
 
         if(mode.equals("Author"))
         {
-            ///Todo implement search function - currently hardcoded
-            // go through all authors and match them with the levenshtein distance
-
-            // Citation Graph contains all Authors and ReferenceAuthors - would we print both?
             Collection<Author> authors = GraphCreator.getInstance().getCitationGraph().getVertices();
             ArrayList<Author> authors_list = new ArrayList<>(authors);
 
@@ -48,67 +45,15 @@ public class Searcher
 
             }
 
-
-
         }
-        else if(mode.equals("Paper"))
-        {
-            ///Todo implement search function - currently hardcoded, watch out that a paper is only shown once
-            // go through all edges and match them with the levenshtein distance
+        else if(mode.equals("Paper")) {
+
             Collection<EdgeCoAuthorship> edges = GraphCreator.getInstance().getCoAuthorGraph().getEdges();
             ArrayList<EdgeCoAuthorship> edges_list = new ArrayList<>(edges);
 
-            ArrayList<String> paper_names_list = new ArrayList<>();
+            test_list = LevenshteinDistancsClass.paperTitelLevenshteinDistances(keyword, edges_list, 20);
 
-
-            for (int i = 0; i < edges_list.size(); i++)
-            {
-                String new_paper = edges_list.get(i).getPapers().toString();
-
-                boolean already_in_array = false;
-
-                for(String s : paper_names_list) {
-
-                    if (s.equals(new_paper))
-                    {
-                        already_in_array = true;
-                    }
-                }
-
-                if (!already_in_array)
-                {
-                    paper_names_list.add(new_paper);
-                }
-            }
-
-            String paper_names[] = new String[paper_names_list.size()];
-
-            int x = 0;
-            for (String s : paper_names_list)
-            {
-                paper_names[x] = s;
-                System.out.println(paper_names[x]);
-
-                x++;
-            }
-
-            String similiar_papers[] = LevenshteinDistancsClass.getTopTenLevenshteinDistances(keyword, paper_names, 15);
-
-            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
-            System.out.println("Papers: " + similiar_papers.length);
-
-            for (int i = 0; i < similiar_papers.length; i++)
-            {
-                System.out.println(similiar_papers[i]);
-
-                // TODO ADD to test_list!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-            }
-
-            test_list.addAll(edges_list.get(0).getPapers());
-            test_list.addAll(edges_list.get(1).getPapers());
         }
-
 
         return test_list;
     }
